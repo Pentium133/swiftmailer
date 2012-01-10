efea227bd915006f3c475668a539b027b3df2229
Serg <github@puhoff.com>
2012-01-10 10:59:56 +0400

    fix open_basedir restriction issue

--- lib/preferences.php
+++ lib/preferences.php
@@ -12,10 +12,10 @@ Swift_Preferences::getInstance()->setCharset('utf-8');
 // Without these lines the default caching mechanism is "array" but this uses
 // a lot of memory.
 // If possible, use a disk cache to enable attaching large attachments etc
-if (function_exists('sys_get_temp_dir') && is_writable(sys_get_temp_dir()))
+if (ini_get('upload_tmp_dir') && is_writable(ini_get('upload_tmp_dir') . '/'))
 {
-  Swift_Preferences::getInstance()
-    -> setTempDir(sys_get_temp_dir())
+  SwiftMailer_Preferences::getInstance()
+    -> setTempDir(ini_get('upload_tmp_dir'))
     -> setCacheType('disk');
 }
 
